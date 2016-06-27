01049704f405ffb4ef1c7cf4b04e37872b54756b
Kurt Hammes <kilowatt11@cableone.net>
2016-06-27 16:32:56 -0600

    redid whole layout using jumbotron and lists

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 //Do Not Modify the getMusic function
-function getMusic(){
-  var artist = document.getElementById('artist').value;
-  itunes.getMusicByArtist(artist).then(drawSongs);
+function getMusic() {
+    var artist = document.getElementById('artist').value;
+    itunes.getMusicByArtist(artist).then(drawSongs);
 }
 
 
 
-function drawSongs(songList){
-  console.log(songList);
+function drawSongs(songList) {
+    console.log(songList);
+    document.getElementsByClassName('artists').innerHTML = '';
+    for (var i = 0; i < songList.length; i++) {
+        var songs = songList[i];
+        var tabs = document.createElement('li');
+        tabs.className = 'jumbotron list-item'
+        tabs.innerHTML += '<h2>' + songs.artist + '<h2>'
+        tabs.innerHTML += '<img class="media-object" src="' + songs.albumArt + '"alt ="..."/>'
+        tabs.innerHTML += '<h4>' + songs.collection + '<h4>'
+        tabs.innerHTML += '<h3>' + songs.title + '<h3>' 
+        tabs.innerHTML += '<audio src="'+ songs.preview + '" preload="auto" controls></audio>'
+        document.getElementById("artists").appendChild(tabs);
   
-  for (var i = 0; i < songList.length; i++) {
-    var songs = songList[i];
- 
-    var tabs = document.createElement('div')
-    var tabs2 = document.createElement('div')
-    tabs.className = 'media'
-    tabs.innerHTML += '<div class = "media-left"><a class="media-left" href="#"><img class="media-object" src="' + songs.albumArt + '"alt ="..."/> </a><br>';
-    tabs.innerHTML += '<div class="media-body"><h4 class="media-heading">' + songs.artist + '</h4><p>' + songs.title + ": " + songs.price + '</p><p><audio src="'+ songs.preview + '" preload="auto" controls></audio><p></div>'
-    document.getElementById("content").appendChild(tabs);
-    
-  } 
-  
-  
+    }
 }
+document.addEventListener('play', function(e){
+   var audios = document.getElementsByTagName('audio');
+   for(var i = 0, len = audios.length; i < len;i++){
+       if(audios[i] != e.target){
+           audios[i].pause();
+       }
+   }
+}, true);
