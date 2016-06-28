85364fd79209f5fd8fff6e6e30d32ff2bba491a0
Kurt Hammes <kilowatt11@cableone.net>
2016-06-27 22:48:24 -0600

    fixed reset after new artist. added rounded corners to images

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ function getMusic() {
 
 function drawSongs(songList) {
     console.log(songList);
-    document.getElementsByClassName('artists').innerHTML = '';
+    document.getElementById('artists').innerHTML = '';
     for (var i = 0; i < songList.length; i++) {
         var songs = songList[i];
         var tabs = document.createElement('li');
         tabs.className = 'jumbotron list-item'
         tabs.innerHTML += '<h2>' + songs.artist + '<h2>'
-        tabs.innerHTML += '<img class="media-object" src="' + songs.albumArt + '"alt ="..."/>'
+        tabs.innerHTML += '<img class="media-object img-rounded" src="' + songs.albumArt + '"alt ="..."/>'
         tabs.innerHTML += '<h4>' + songs.collection + '<h4>'
         tabs.innerHTML += '<h3>' + songs.title + '<h3>' 
         tabs.innerHTML += '<audio src="'+ songs.preview + '" preload="auto" controls></audio>'
